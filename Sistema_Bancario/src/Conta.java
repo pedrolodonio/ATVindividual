3b0b4d7150af0ba5f0798bb5b7dc9907e77a7617
pedrolodonio <pedrolodonio20@gmail.com>
2024-09-26 02:04:24 -0300

    adiciona método de saque na classe conta

--- Sistema_Bancario/src/Conta.java
+++ Sistema_Bancario/src/Conta.java
@@ -10,7 +10,7 @@ public class Conta {
 
     }
 
-    public synchronized void depositar (double dinheiro){
+    public synchronized void deposito (double dinheiro){
 
         if (dinheiro > 0){
 
@@ -20,9 +20,28 @@ public class Conta {
 
     } 
     else {
-        System.out.println("este valor não é válido para a realização do depósito");
+        System.out.println("Este valor não é válido para a realização do depósito");
+    }
     }
 
+    public synchronized boolean saque (double dinheiro){
+
+        if (dinheiro > 0 && saldo >= dinheiro) {
+
+            saldo -= dinheiro ;
+
+            System.out.println(Thread.currentThread()+"Você sacou R$" + dinheiro + "da sua conta. Seu saldo foi atualizado para R$" + saldo);
+
+            return true ;
+        }
+    
+        else{
+
+            System.out.println(Thread.currentThread() + "saldo insuficiente para realizar a operação de saque. Seu saldo atual é de R$" + saldo);
+
+            return false;
+        }
+    }
 
 }
-}
+

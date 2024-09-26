d61357829dd33afbb55aa23f0edcf89f134c6ca1
pedrolodonio <pedrolodonio20@gmail.com>
2024-09-26 17:31:35 -0300

    adicionando atributos da classe funcionarios e métodos recebeSalario e investimento

--- Sistema_Bancario/src/Funcionario.java
+++ Sistema_Bancario/src/Funcionario.java
@@ -1,3 +1,26 @@
 public class Funcionario {
 
+    private String nome;
+    private Conta contaCorrente;
+    private Conta contaInvestimentos;
+
+    public Funcionario (String nome){
+        this.nome = nome;
+        this.contaCorrente = new Conta(0.0);
+        this.contaInvestimentos = new Conta (0.0);
+    }
+
+    public synchronized void investimento (double dinheiro){
+
+        contaInvestimentos.deposito(dinheiro);
+        System.out.println(nome+ "fez um investimento de R$" + dinheiro);
+    }
+
+    public synchronized void  recebeSalario (double dinheiro){
+
+        contaCorrente.deposito(dinheiro);
+        System.out.println(nome+ "recebeu R$" + dinheiro);
+        investimento(0.20*dinheiro);
+    }
+
 }

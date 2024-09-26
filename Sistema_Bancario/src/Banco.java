f5d48a292d338bd49e7cabd597768d7729a38827
pedrolodonio <pedrolodonio20@gmail.com>
2024-09-26 16:46:05 -0300

    adicionando método de tranferência na classe banco

--- Sistema_Bancario/src/Banco.java
+++ Sistema_Bancario/src/Banco.java
@@ -1,3 +1,19 @@
 public class Banco {
 
+    public Banco(){
+
+    }
+
+    public synchronized void transferência (Conta remetente, Conta beneficiario, double dinheiro){
+
+        if(remetente.saque(dinheiro)){
+            beneficiario.deposito(dinheiro);
+            System.out.println("Você transferiu R$ " + dinheiro + "para" + beneficiario );
+        } 
+        
+        else{
+            System.out.println("saldo insuficiente para realização da transferência");
+        }
+
+    }
 }

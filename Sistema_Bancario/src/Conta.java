5f158fe5240d7bade7bc400db9f8d61afba2dfe0
pedrolodonio <pedrolodonio20@gmail.com>
2024-09-26 01:50:06 -0300

    adiciona atributos da classe Conta e o método depositar

--- Sistema_Bancario/src/Conta.java
+++ Sistema_Bancario/src/Conta.java
@@ -1,3 +1,28 @@
+
+
 public class Conta {
 
+    private double saldo ;
+
+    public Conta ( double saldo_inicial){
+
+        this.saldo = saldo_inicial ;
+
+    }
+
+    public synchronized void depositar (double dinheiro){
+
+        if (dinheiro > 0){
+
+        saldo += dinheiro ;
+
+        System.out.println(Thread.currentThread()+"foram depositados R$ " + dinheiro + "na sua conta");
+
+    } 
+    else {
+        System.out.println("este valor não é válido para a realização do depósito");
+    }
+
+
+}
 }

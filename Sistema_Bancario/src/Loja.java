a7fe492263128cb835916e7e3fabbe02560f669d
pedrolodonio <pedrolodonio20@gmail.com>
2024-09-27 01:25:07 -0300

    adicionando atributos da classe Loja

--- Sistema_Bancario/src/Loja.java
+++ Sistema_Bancario/src/Loja.java
@@ -1,3 +1,15 @@
 public class Loja {
 
+    private Conta contaLoja;
+    private Funcionario [] funcionarios ;
+
+    public Loja (double saldo_inicial){
+
+        this.contaLoja = new Conta(0.0);
+        this.funcionarios = new Funcionario [2];
+    }
+
+    
+
+
 }

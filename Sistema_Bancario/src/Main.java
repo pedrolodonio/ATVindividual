56146a1c3539ca07454ed6db32ae1be80c67bd16
pedrolodonio <pedrolodonio20@gmail.com>
2024-09-27 15:05:59 -0300

    adicionando funcionários, eftuando suas contratações e iniciando as threads

--- Sistema_Bancario/src/Main.java
+++ Sistema_Bancario/src/Main.java
@@ -9,10 +9,20 @@ public class Main {
         Loja LojaX = new Loja("Loja X", 0.0);
         Loja LojaY = new Loja("Loja Y", 0.0);
 
+        Funcionario funcionario1 = new Funcionario("Chloe");
+        Funcionario funcionario2 = new Funcionario("Sam");
+        Funcionario funcionario3 = new Funcionario("Sofia");
+        Funcionario funcionario4 = new Funcionario("Marcus");
+
+        LojaX.contratacao(funcionario1, 0);
+        LojaX.contratacao(funcionario2, 1);
+        LojaY.contratacao(funcionario3, 0);
+        LojaY.contratacao(funcionario4, 1);
+
         List<String> nomesClientes = new ArrayList<> ();
         nomesClientes.add("Taylor");
         nomesClientes.add("James");
-        nomesClientes.add("Augusta");
+        nomesClientes.add("Augustine");
         nomesClientes.add("Betty");
         nomesClientes.add("Inês");
         nomesClientes.add("Erica");
@@ -29,6 +39,13 @@ public class Main {
             clientes[i].start();
 
         }
+
+        funcionario1.start();
+        funcionario2.start();
+        funcionario3.start();
+        funcionario4.start();
+
+        
         
 
     }
